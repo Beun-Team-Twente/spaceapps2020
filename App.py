18114e29c465e49ce80dae85614630cbe70806c3
Antonis Katzourakis <a.katzourakis@greenteamtwente.nl>
2020-10-03 20:19:13 +0200

    Debugging off

--- App.py
+++ App.py
@@ -78,18 +78,18 @@ def request_drawings():
             "error": f"Error while requesting site: {e}"
             })
 
-# DEBUGGING: Disable cache
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+# # DEBUGGING: Disable cache
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     return r
 
 @app.teardown_appcontext
 def close_database_connection(exception): # Automatically close the DB when stopping the App
