6fefc87ec8dec96aa70b9e5f7af95beeb0743f6b
Antonis Katzourakis <a.katzourakis@greenteamtwente.nl>
2020-10-04 11:54:41 +0200

    Add astronaut integrated #2

--- App.py
+++ App.py
@@ -25,34 +25,34 @@ def upload_file():
     drawing_blob = ""
     drawing_id = 0
     rockets = [] # [{'data': base64-image, 'location': location}, ..]
-    # try:
-    f = request.files['drawing']
-    img = request.files['drawing'].read()
-    img = Image.open(io.BytesIO(img)).convert('RGB')
+    try:
+        f = request.files['drawing']
+        img = request.files['drawing'].read()
+        img = Image.open(io.BytesIO(img)).convert('RGB')
 
-    # Process with the computer vision module.
-    img = get_rocket.run(img)
-    if img is None:
-        # No contours found:
+        # Process with the computer vision module.
+        img = get_rocket.run(img)
+        if img is None:
+            # No contours found:
+            return json.dumps({
+                'error':'No contours'
+                })
+
+        # Get astronaut
+        astronaut = get_astronaut.run(img)
+        astronaut_blob = 'data:image/jpeg;base64,' + drawing_handler.get_drawing_blob(astronaut)
+
+        drawing_id, drawing_blob, other_drawings = drawing_handler.store(img)
+        drawing_blob = 'data:image/jpeg;base64,' + drawing_blob
+
+        for r in range(len(other_drawings)):
+            other_drawings[r]['drawing'] = 'data:image/jpeg;base64,' + other_drawings[r]['drawing'].decode('utf-8')
+            other_drawings[r]['location'] = [int(i) for i in other_drawings[r]['location'].replace(" ","").split(",")]
+    except Exception as e:
+        # return json.dumps({'error': str(e)})
         return json.dumps({
-            'error':'No contours'
-            })
-
-    # Get astronaut
-    astronaut = get_astronaut.run(img)
-    astronaut_blob = 'data:image/jpeg;base64,' + drawing_handler.get_drawing_blob(astronaut)
-
-    drawing_id, drawing_blob, other_drawings = drawing_handler.store(img)
-    drawing_blob = 'data:image/jpeg;base64,' + drawing_blob
-
-    for r in range(len(other_drawings)):
-        other_drawings[r]['drawing'] = 'data:image/jpeg;base64,' + other_drawings[r]['drawing'].decode('utf-8')
-        other_drawings[r]['location'] = [int(i) for i in other_drawings[r]['location'].replace(" ","").split(",")]
-    # except Exception as e:
-    #     # return json.dumps({'error': str(e)})
-    #     return json.dumps({
-    #             'error':'No contours' # Will alert to try reuploading
-    #             })
+                'error':'No contours' # Will alert to try reuploading
+                })
 
     return json.dumps({
             'error':'',
